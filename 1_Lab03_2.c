38e21a2271cdb30222ee0eb9e848ac326c2243d2
“seyi0000” <csy040211@gmail.com>
2023-05-11 14:48:47 +0900

    1_Lab03_1.c

---
+++ 1_Lab03_2.c
@@ -0,0 +1,16 @@
+//2753
+
+#include <stdio.h>
+
+int main(void) {
+  int year;
+  scanf("%d",&year);
+  if(year%4==0 && year%100!=0 || year%400==0){
+    printf("%d\n",1);
+  }
+  else{
+    printf("%d\n",0);
+
+  }
+  return 0;
+}
